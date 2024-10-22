39560ccdfd8a362f546f2ef3a26f8f4ab01c6cb4
belfortep <pbelforte2003@gmail.com>
2024-10-22 15:11:03 -0300

    Agregar libros favoritos en usuario

--- api/src/controllers/bookController.js
+++ api/src/controllers/bookController.js
@@ -1,4 +1,6 @@
 const Book = require('../models/Book');
+const User = require('../models/User');
+
 const { HttpCodesEnum } = require('../enum/httpCodes');
 
 const getAllBooks = async (req, res) => {
@@ -20,7 +22,25 @@ const getBookById = async (req, res) => {
     }
 }
 
+const addBookToFavoriteById = async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        if (!book) {
+            return res.status(HttpCodesEnum.NOT_FOUND).json({message: "Libro no encontrado"})
+        }
+        const currentUser = await User.findById(req.body.user_id);
+        if (currentUser.books.includes(req.params.id)) {
+            return res.status(HttpCodesEnum.FORBBIDEN).json({ message: "Ya tienes este libro en favoritos" });
+        }
+        await currentUser.updateOne({$push: {books: req.params.id}})
+        return res.status(HttpCodesEnum.OK).json("Libro agregado")
+    } catch (err) {
+        return res.status(HttpCodesEnum.SERVER_INTERNAL_ERROR).json({ message: err.message });
+    }
+}
+
 module.exports = {
     getBookById,
     getAllBooks,
+    addBookToFavoriteById
 }
--- api/src/routes/book.js
+++ api/src/routes/book.js
@@ -1,12 +1,13 @@
 const Router = require('express')
 const router = Router();
-const { getAllBooks, getBookById } = require('../controllers/bookController');
+const { getAllBooks, getBookById, addBookToFavoriteById } = require('../controllers/bookController');
 const { verifyUser } = require('../util/verifyToken');
 
 
 
 router.get('/', verifyUser, getAllBooks);
 router.get('/:id', verifyUser, getBookById);
+router.get('/fav/:id', verifyUser, addBookToFavoriteById)
 
 
 module.exports = router
