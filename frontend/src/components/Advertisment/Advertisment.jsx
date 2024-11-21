1797f6e6d1716c4b23c2912fb1daf34dcd37f0c9
ipuj <ipujato@fi.uba.ar>
2024-11-20 22:33:26 -0300

    fix ad

--- frontend/src/components/Advertisment/Advertisment.jsx
+++ frontend/src/components/Advertisment/Advertisment.jsx
@@ -7,8 +7,10 @@ export const Advertisment = () => {
   return (
     <>
       {(!user || !user.isPremium) && (
-        <div className="horizontal-banner" style={{ backgroundColor: "black", height: "100px", color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <div className="horizontal-banner" style={{ backgroundColor: "red", height: "100px", color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>
           <div style={{ display: "flex", flexWrap: "nowrap", overflow: "hidden", height: "100px", width: "100%" }}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKyh8oCOn6SrqJHiDBcQcG529zWdHoFbzTw&s" alt="Advertisement" style={{ height: "100%", flexShrink: 0 }} />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKyh8oCOn6SrqJHiDBcQcG529zWdHoFbzTw&s" alt="Advertisement" style={{ height: "100%", flexShrink: 0 }} />
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKyh8oCOn6SrqJHiDBcQcG529zWdHoFbzTw&s" alt="Advertisement" style={{ height: "100%", flexShrink: 0 }} />
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKyh8oCOn6SrqJHiDBcQcG529zWdHoFbzTw&s" alt="Advertisement" style={{ height: "100%", flexShrink: 0 }} />
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKyh8oCOn6SrqJHiDBcQcG529zWdHoFbzTw&s" alt="Advertisement" style={{ height: "100%", flexShrink: 0 }} />
