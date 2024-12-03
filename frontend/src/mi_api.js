4f194dc5ac42db9fefa40ada265e3d933f5e0e71
belfortep <pbelforte2003@gmail.com>
2024-12-03 20:56:37 -0300

    uso http no https

--- frontend/src/mi_api.js
+++ frontend/src/mi_api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'https://librardos-server.vercel.app', // Cambia al puerto de tu API
+    baseURL: 'http://librardos-server.vercel.app', // Cambia al puerto de tu API
 });
 
 export default api;
